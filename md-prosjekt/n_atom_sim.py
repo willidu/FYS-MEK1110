7dbeae31ac3f0714d77bb8cce45369f94f43c59e
willidu <95777710+willidu@users.noreply.github.com>
2022-02-10 23:19:26 +0100

    implemented acceleration

--- md-prosjekt/n_atom_sim.py
+++ md-prosjekt/n_atom_sim.py
@@ -26,14 +26,21 @@ class System:
         self.r0 = np.asarray(r0)
         self.v0 = np.asarray(v0)
 
-    def a(r, t):
-        pass
+    def a(self, x, t):
+        a = np.zeros((len(x), len(x), self.dim))
+        for i in range(len(x)):
+            for j in range(i+1, len(x)):
+                r = x[i] - x[j]
+                r_norm = np.linalg.norm(r)
+                a[i,j] = -(24*(2*(r_norm)**(-12) - (r_norm)**(-6)) * (r) / (r_norm)**2)
+                a[j,i] = -a[i,j]
+        return np.sum(a, axis=0)
 
     def solve(self, T, dt):
         # Using the Velocity Verlet integration method
         numpoints = int(np.ceil(T/dt))
         t = np.linspace(0, T, numpoints)
-        x = np.zeros((self.n, self.dim))
+        x = np.zeros((numpoints, self.n, self.dim))
         v = np.zeros_like(x)
         x[0] = self.r0; v[0] = self.v0
 
@@ -46,6 +53,15 @@ class System:
 
         return t, x, v
 
-r0 = ((1, 2), (3, 3))
-v0 = ((0, 0), (0, 0))
-s1 = System(r0 , v0, 3, 2, test=True)
+if __name__ == '__main__':
+    r0 = [[0, 0], [1.5, 0]]
+    v0 = np.zeros_like(r0)
+    s1 = System(r0 , v0, 2, 2, test=True)
+    t, x, v = s1.solve(5, 0.01)
+
+    print(x[0])
+
+    d = x[:,1] - x[:,0]
+    d = d[:,0]
+    plt.plot(t, d)
+    plt.show()
