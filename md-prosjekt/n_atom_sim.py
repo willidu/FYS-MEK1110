9a8e76fff15757d94750803ba9358f162560b005
willidu <95777710+willidu@users.noreply.github.com>
2022-02-10 21:28:08 +0100

    added solver

--- md-prosjekt/n_atom_sim.py
+++ md-prosjekt/n_atom_sim.py
@@ -26,6 +26,26 @@ class System:
         self.r0 = np.asarray(r0)
         self.v0 = np.asarray(v0)
 
+    def a(r, t):
+        pass
+
+    def solve(self, T, dt):
+        # Using the Velocity Verlet integration method
+        numpoints = int(np.ceil(T/dt))
+        t = np.linspace(0, T, numpoints)
+        x = np.zeros((self.n, self.dim))
+        v = np.zeros_like(x)
+        x[0] = self.r0; v[0] = self.v0
+
+        a_ = self.a(x[0], t[0])
+        for i in range(numpoints-1):
+            x[i+1] = x[i] + v[i]*dt + 0.5*a_*dt**2
+            a_2 = self.a(x[i+1], t[i+1])
+            v[i+1] = v[i] + 0.5*(a_ + a_2)*dt
+            a_ = a_2
+
+        return t, x, v
+
 r0 = ((1, 2), (3, 3))
 v0 = ((0, 0), (0, 0))
 s1 = System(r0 , v0, 3, 2, test=True)
