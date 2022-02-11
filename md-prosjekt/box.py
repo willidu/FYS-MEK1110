54e38c9d0a37e9e247fa102f2a6f54aca32ec81b
willidu <95777710+willidu@users.noreply.github.com>
2022-02-11 23:16:18 +0100

    moved tasks to separate files

--- md-prosjekt/box.py
+++ md-prosjekt/box.py
@@ -1,5 +1,4 @@
 import numpy as np
-from n_atom_sim import System
 
 def unitcell(i, j, k, d):
     unit_cell = d * np.asarray(
@@ -22,36 +21,3 @@ def lattice(n, L):
                 pos[index:index+4] = unitcell(i, j, k, d)
                 index += 4
     return pos
-
-def task_3c():
-    positions = lattice(n=3, L=20)
-
-    with open('lattice_test.xyz', 'w') as file:
-        file.write(f'{len(positions)} \n')
-        file.write(f'type  x  y  z\n')
-        for r_ in positions:
-            file.write(f'Ar   {r_[0]}  {r_[1]}  {r_[2]}\n')
-
-def task_3d():
-    r0 = lattice(n=4, L=1.7*4)
-    v0 = np.zeros_like(r0)
-    s = System(r0, v0, 256, 3, True)
-    print('System initiated')
-    t, x, v = s.solve(5, 0.01)
-    print('Simulation complete, writing to file.')
-    s.write__xyz_file('task_3d.xyz', x)
-    print('Energy calculations started')
-    s.energy(show=True)
-    print('Done!')
-
-def task_3e():
-    r0 = [1, 0, 0]
-    v0 = [1, 0, 0]
-    s = System(r0, v0, 1, 3, L=3, test=False)
-    t, x, v = s.solve(15, 0.01)
-    s.write__xyz_file('task_3e.xyz', x)
-
-if __name__ == '__main__':
-    # task_3c()
-    # task_3d()
-    task_3e()
