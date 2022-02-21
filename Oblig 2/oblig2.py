f7d5d5ca11ae667674db44ddf48542676268ad4b
willidu <95777710+willidu@users.noreply.github.com>
2022-02-21 21:40:13 +0100

    new plots

--- Oblig 2/oblig2.py
+++ Oblig 2/oblig2.py
@@ -13,18 +13,20 @@ T = 10
 dt = 0.001
 n = int(np.ceil(T/dt))
 
-t = np.linspace(0, T, n)
-r = np.zeros((n, 2)); r[0] = r0
-v = np.zeros_like(r)
-a = np.zeros_like(r)
+for k in (20, 2000):
+    plt.clf()
+    t = np.linspace(0, T, n)
+    r = np.zeros((n, 2)); r[0] = r0
+    v = np.zeros_like(r)
+    a = np.zeros_like(r)
 
-for i in range(n-1):
-    r_ = np.linalg.norm(r[i])
-    a[i] = -k*(1-L0/r_)/m * r[i,0], -g-k*(1-L0/r_)/m * r[i, 1]
-    v[i+1] = v[i] + a[i]*dt
-    r[i+1] = r[i] + v[i+1]*dt
+    for i in range(n-1):
+        r_ = np.linalg.norm(r[i])
+        a[i] = -k*(1-L0/r_)/m * r[i,0], -g-k*(1-L0/r_)/m * r[i, 1]
+        v[i+1] = v[i] + a[i]*dt
+        r[i+1] = r[i] + v[i+1]*dt
 
-plt.plot(r[:,0], r[:,1])
-plt.xlabel('x [m]')
-plt.ylabel('y [m]')
-plt.show()
+    plt.plot(r[:,0], r[:,1])
+    plt.xlabel('x [m]')
+    plt.ylabel('y [m]')
+    plt.savefig(f'k{k}.pdf')
