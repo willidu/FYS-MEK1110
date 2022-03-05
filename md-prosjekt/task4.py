c1cfccf0a5b562286c68b087968fbe46963e749e
willidu <95777710+willidu@users.noreply.github.com>
2022-03-05 19:14:10 +0100

    oppgave 4a

--- md-prosjekt/task4.py
+++ md-prosjekt/task4.py
@@ -6,10 +6,10 @@ def task_4_ii():
     r0 = lattice(n=3, L=1.7*3)
     v0 = np.zeros_like(r0)
     
-    s = System(r0, v0, 108, L=1.7*4, bound=True, rc=3, test=True)
+    s = System(r0, n=108, L=1.7*4, bound=True, rc=3, test=True)
     s.set_inital_velocities(T=300)
     t, x, v = s.solve(5, 0.01)
-    # s.write_xyz_file('4_a_ii.xyz')
+    s.write_xyz_file('4_a_ii.xyz')
     temp = s.get_temperature()
     print(temp[0])
 
